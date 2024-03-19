52b5a4fde675a32921187b129683944d391fe762
arnold <arnoldkmaina@gmail.com>
2024-03-19 16:01:47 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result = 'Give customer free sample';
+  if (someValue <= 400) {
+    result = "This one is on me!";
+     return result
+  }
+  else if (someValue > 400 && someValue < 2000)
+  {
+    result = "That will be twenty bucks.";
+    return result
+  }
+  else if (someValue > 2000 && someValue <2500)
+  {
+    result = "I will gladly take your thirty bucks.";
+    return result
+  }
+  else if (someValue > 2500)
+  {
+    result = "No can do."
+    return result
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'? "Ok, sounds good.": "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip)
+  {
+    case "generous" :
+      return "Thank you so much."
+      case "not as generous":
+      return  "Thank you."
+      default :
+      return "Bye."
+  }
+  
 }
+  
